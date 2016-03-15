d8063eda9da99861db35a9dcc257def85c308cae
Aobo <aobozhang@gmail.com>
2016-03-15 22:21:01 +0800

    阶段性保存，完成样式初调，使用自定义sass

--- www/js/app.js
+++ www/js/app.js
@@ -5,85 +5,117 @@
 // the 2nd parameter is an array of 'requires'
 // 'dreampicker.services' is found in services.js
 // 'dreampicker.controllers' is found in controllers.js
-angular.module('dreampicker', [
-  'ionic',
-  'dreampicker.controllers',
-  'dreampicker.services'
+var DpApp = angular.module('dreampicker', [
+    'ionic',
+    'ngResource',
+    'dreampicker.controllers',
+    'dreampicker.services'
 ])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+    $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
+    });
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
 
-  // setup an abstract state for the tabs directive
-  .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
-  })
+    // setup an abstract state for the tabs directive
+    .state('tab', {
+        url: '/tab',
+        abstract: true,
+        templateUrl: 'templates/tabs.html'
+    })
 
-  // Each tab has its own nav history stack:
+    // Each tab has its own nav history stack:
 
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'BrowserCtrl'
-      }
-    }
-  })
+    .state('tab.dash', {
+        url: '/dash',
+        views: {
+            'tab-dash': {
+                templateUrl: 'templates/tab-dash.html',
+                controller: 'BrowserCtrl'
+            }
+        }
+    })
 
-  .state('tab.chats', {
-    url: '/chats',
-    views: {
-      'tab-chats': {
-        templateUrl: 'templates/tab-chats.html',
-        controller: 'ChatsCtrl'
-      }
-    }
-  })
-  .state('tab.chat-detail', {
-    url: '/chats/:chatId',
-    views: {
-      'tab-chats': {
-        templateUrl: 'templates/chat-detail.html',
-        controller: 'ChatDetailCtrl'
-      }
-    }
-  })
+    .state('tab.chats', {
+        url: '/chats',
+        views: {
+            'tab-chats': {
+                templateUrl: 'templates/tab-chats.html',
+                controller: 'ChatsCtrl'
+            }
+        }
+    })
 
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
+    .state('tab.dreams', {
+        url: '/dreams',
+        views: {
+            'tab-dreams': {
+                templateUrl: 'templates/tab-dreams.html',
+                controller: 'DreamsCtrl'
+            }
+        }
+    })
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+    .state('tab.chat-detail', {
+        url: '/chats/:chatId',
+        views: {
+            'tab-chats': {
+                templateUrl: 'templates/chat-detail.html',
+                controller: 'ChatDetailCtrl'
+            }
+        }
+    })
+
+    .state('tab.dream-detail', {
+        url: '/dreams/:dreamId',
+        views: {
+            'tab-dreams': {
+                templateUrl: 'templates/dream-detail.html',
+                controller: 'DreamDetailCtrl'
+            }
+        }
+    })
+
+    .state('tab.create', {
+        url: '/create',
+        views: {
+            'tab-create': {
+                templateUrl: 'templates/tab-create.html',
+                controller: 'CreateCtrl'
+            }
+        }
+    })
+
+    .state('tab.account', {
+        url: '/account',
+        views: {
+            'tab-account': {
+                templateUrl: 'templates/tab-account.html',
+                controller: 'AccountCtrl'
+            }
+        }
+    });
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/tab/dash');
 
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,44 +3,86 @@ angular.module('dreampicker.controllers', [])
 .controller('DashCtrl', function($scope) {})
 
 .controller('BrowserCtrl', function($scope, $ionicSideMenuDelegate) {
-  $scope.toggleProjects = function() {
-    $ionicSideMenuDelegate.toggleLeft();
-  };
+    $scope.toggleProjects = function() {
+        $ionicSideMenuDelegate.toggleLeft();
+    };
 })
 
 .controller('TodoCtrl', function($scope, $ionicSideMenuDelegate) {
-  $scope.tasks = [
-    { title: '菜鸟教程' },
-    { title: 'www.runoob.com' },
-    { title: '菜鸟教程' },
-    { title: 'www.runoob.com' }
-  ];
-  $scope.toggleProjects = function() {
-    $ionicSideMenuDelegate.toggleLeft();
-  };
+    $scope.tasks = [
+        { title: '菜鸟教程' },
+        { title: 'www.runoob.com' },
+        { title: '菜鸟教程' },
+        { title: 'www.runoob.com' }
+    ];
+    $scope.toggleProjects = function() {
+        $ionicSideMenuDelegate.toggleLeft();
+    };
 })
 
 .controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
+    $scope.chats = Chats.all();
+
+    $scope.remove = function(chat) {
+        Chats.remove(chat);
+    };
+})
+
+.controller('DreamsCtrl', function($scope, $http, Dream) {
+
+    $scope.$on('$ionicView.enter', function(e) {
+        $http.jsonp('http://dream.atreehole.com/api/dream?callback=JSON_CALLBACK').success(function(data, status, headers, config){
+            $scope.page = data
+            $scope.dreams = data.data;
+        });
+
+    });
+
+    $http.jsonp('http://dream.atreehole.com/api/dream?callback=JSON_CALLBACK').success(function(data, status, headers, config){
+        $scope.page = data
+        $scope.dreams = data.data;
+    });
+
+    $scope.delete = function(dream) {
+        dreams.delete(dream);
+    };
+
+    $scope.doRefresh = function() {
+        $http.jsonp('http://dream.atreehole.com/api/dream?callback=JSON_CALLBACK')
+        .success(function(data, status, headers, config){
+            $scope.page = data
+            $scope.dreams = data.data;
+        })
+        .finally(function() {
+            $scope.$broadcast('scroll.refreshComplete');
+        });
+    };
+    $scope.redirectTo = function(index) {
+        location.href="#/tab/dreams/" + index;
+    };
+})
+
+.controller('DreamDetailCtrl', function($scope, $http, $stateParams) {
+    $http.jsonp('http://dream.atreehole.com/api/dream/' + $stateParams.dreamId + '?callback=JSON_CALLBACK').success(function(data, status, headers, config){
+        $scope.dream = data;
+    });
+
 })
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+    $scope.chat = Chats.get($stateParams.chatId);
 })
 
 .controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+    $scope.settings = {
+        enableFriends: true
+    };
 });
